102a2eeb8544a91da7b6ae71a7e8188a14a13c9f
Arthur Borges <arthurborges.dev@gmail.com>
2021-04-26 19:59:23 -0300

    Add relationship between Post and User models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: { message: "Can't be empty" }
   validates :body, presence: { message: "Can't be empty" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  
   validates :username, presence: true, length: { minimum: 4 }, uniqueness: true
   validates :email, presence: true, format: { with: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: 'Email must be valid'}
 end
