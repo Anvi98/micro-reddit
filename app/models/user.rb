f771f39e126278b5087c8292832240e2a97e300c
Anvi Alex <epononanvialex@gmail.com>
2021-04-27 01:22:57 +0300

    Add validations on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { minimum: 4 }, uniqueness: true
+  validates :email , presence: true, format: { with: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: 'Email must be valid'}
 end
