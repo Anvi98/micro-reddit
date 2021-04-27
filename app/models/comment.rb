2b9ebd1b33b78b1eaa42fe70885989291791d5e4
Anvi Alex <epononanvialex@gmail.com>
2021-04-27 22:45:28 +0300

    Adding Associations and validations between tables

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  validates_associated :user
+  validates_associated :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
 
   validates :title, presence: { message: "Can't be empty" }
   validates :body, presence: { message: "Can't be empty" }
